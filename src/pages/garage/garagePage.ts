663091188f1f34b300edc20fbc01fed263e6b961
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-01-15 21:43:03 +0100

    feat: add basic layout to garage page

--- src/pages/garage/garagePage.ts
+++ src/pages/garage/garagePage.ts
@@ -1,10 +1,11 @@
 import Header from '../../components/header/header';
+import './garage.css';
 class GaragePage {
-    container: HTMLDivElement;
+    container: HTMLElement;
     header: Header;
 
     constructor() {
-        this.container = document.createElement('div');
+        this.container = document.createElement('main');
         this.container.className = 'garage';
         this.header = new Header();
     }
@@ -14,8 +15,91 @@ class GaragePage {
 
         const createBlock = document.createElement('div');
         createBlock.className = 'garage__create create';
-        createBlock.textContent = 'Garage';
         this.container.append(createBlock);
+
+        const nameInput = document.createElement('input');
+        nameInput.className = 'create__name';
+        nameInput.type = 'text';
+        nameInput.placeholder = 'Enter car\'s name';
+        createBlock.append(nameInput);
+
+        const colorInput = document.createElement('input');
+        colorInput.className = 'create__color';
+        colorInput.type = 'color';
+        createBlock.append(colorInput);
+
+        const createButton = document.createElement('button');
+        createButton.className = 'create__button';
+        createButton.textContent = 'Create';
+        createBlock.append(createButton);
+
+        const updateBlock = document.createElement('div');
+        updateBlock.className = 'garage__update update';
+        this.container.append(updateBlock);
+
+        const updateNameInput = document.createElement('input');
+        updateNameInput.className = 'update__name';
+        updateNameInput.type = 'text';
+        updateNameInput.placeholder = 'Enter new name';
+        updateBlock.append(updateNameInput);
+
+        const updateColorInput = document.createElement('input');
+        updateColorInput.className = 'update__color';
+        updateColorInput.type = 'color';
+        updateBlock.append(updateColorInput);
+
+        const updateButton = document.createElement('button');
+        updateButton.className = 'update__button';
+        updateButton.textContent = 'Update';
+        updateBlock.append(updateButton);
+
+        const garageButtons = document.createElement('div');
+        garageButtons.className = 'garage__buttons';
+        this.container.append(garageButtons);
+
+        const raceButton = document.createElement('button');
+        raceButton.className = 'garage__button button_race';
+        raceButton.textContent = 'Race';
+        garageButtons.append(raceButton);
+
+        const resetButton = document.createElement('button');
+        resetButton.className = 'garage__button button_reset';
+        resetButton.textContent = 'Reset';
+        garageButtons.append(resetButton);
+
+        const generateButton = document.createElement('button');
+        generateButton.className = 'garage__button button_generate';
+        generateButton.textContent = 'Generate cars';
+        garageButtons.append( generateButton);
+
+        const carsCounter = document.createElement('div');
+        carsCounter.className = 'garage__counter';
+        this.container.append(carsCounter);
+
+        const title = document.createElement('h1');
+        title.className = 'garage__title';
+        title.textContent = 'Garage';
+        carsCounter.append(title);
+
+        const carsNumber = document.createElement('span');
+        carsNumber.className = 'garage__number';
+        carsNumber.textContent = '()';
+        carsCounter.append(carsNumber);
+
+        const pageCounter = document.createElement('div');
+        pageCounter.className = 'page__counter';
+        this.container.append(pageCounter);
+
+        const page = document.createElement('h2');
+        page.className = 'garage__page';
+        page.textContent = 'Page #';
+        pageCounter.append(page);
+
+        const pageNumber = document.createElement('span');
+        pageNumber.className = 'garage__page-number';
+        pageNumber.textContent = '';
+        pageCounter.append(pageNumber);
+
 
         return this.container;
     }
